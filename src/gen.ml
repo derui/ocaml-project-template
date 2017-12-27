3f6991e262efafe31340ab240fb0a27ac8a8363a
derui <derutakayu@gmail.com>
2017-12-27 09:11:01 +0900

    Add feature to generate configuration for ocp-indent

--- src/gen.ml
+++ src/gen.ml
@@ -1,5 +1,20 @@
 open Cmdliner
 
+let ocp_indent_template = {|
+base = 2
+type = 2
+in = 0
+with = 0
+match_clause = 2
+ppx_stritem_ext = 2
+max_indent = 4
+strict_with = never
+strict_else = always
+strict_comments = false
+align_ops = true
+align_params = auto
+|}
+
 let opam_template = {|
 opam-version: "1.2"
 version: "0.1.0"
@@ -29,12 +44,11 @@ let topkg_template = {|
 
 let project =
   let doc = "Target directory for project" in
-  Arg.(value & opt string "." & info ["p"; "project"] ~docv:"proejct" ~doc)
+  Arg.(value & opt string "." & info ["p"; "project"] ~docv:"project" ~doc)
 
 let license =
   let doc = "The LISENCE of project" in
   Arg.(value & opt string "MIT" & info ["";"lisence"] ~docv:"LISENCE" ~doc)
-
 
 let gen_topkg project =
   let pkg_path = Filename.concat project "pkg" in
@@ -47,26 +61,37 @@ let gen_topkg project =
   let os = open_out pkg_name in
   try
     output_string os topkg_template
-  with _ -> close_out os
+  with _ -> ();
+    close_out os
 
 let gen_opam project license =
   let regex = Str.regexp "<license>" in
   let license_replaced = Str.replace_first regex license opam_template in
-  
+
   if not @@ Sys.file_exists project then
     Term.exit ~term_err:1 (`Error `Exn)
   else
     ();
-    
+
   let project_name = Filename.basename project in
   let os = open_out @@ Filename.(concat project (project_name ^ ".opam")) in
   try
     output_string os license_replaced
-  with _ -> close_out os
+  with _ -> ();
+    close_out os
+
+let gen_ocp_indent project =
+  let config_name = Filename.concat project ".ocp-indent" in
+  let os = open_out config_name in
+  try
+    output_string os ocp_indent_template
+  with _ -> ();
+    close_out os
 
 let gen project license =
   gen_opam project license;
-  gen_topkg project
+  gen_topkg project;
+  gen_ocp_indent project
 
 let gen_t = Term.(const gen $ project $ license)
 
